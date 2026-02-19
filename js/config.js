503d8f41f1c2aabbe47a0c239d6032a229159a2a
janetlin5 <janetlin199702@gmail.com>
2026-02-19 01:01:20 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_N4cjKFLM3',
+        userPoolClientId: '3bn4ok1hksjq8i74dpq8h0c8fc',
+        region: 'us-west-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ''
     }
 };
