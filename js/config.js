1eb813a89d27857ffc9239a66d850445f19a62bc
janetlin5 <janetlin199702@gmail.com>
2026-03-09 06:09:31 +0000

    Add Cognito configuration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_9l8Ilm15x',
+        userPoolClientId: '3fkch46deg5efpnas29qgl86rl', 
+        region: 'us-west-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
